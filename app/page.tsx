adbdedf627839415ad42726bff7af0a580ced979
mujtaba chandio <mujtabachandio384@gmail.com>
2025-04-09 21:31:46 +0500

    chatbot

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,19 @@ import Footer from "./sections/Footer";
 export default function Example() {
   return (
     <>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/67f6a0681340201907628c73/1iodm56hq';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
     <main
       className="bg-white bg-cover bg-center relative"
       style={{ backgroundImage: "url('/bg1.jpg')" }}
