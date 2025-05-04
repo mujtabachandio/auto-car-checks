bcfa2a68622a4979a8e0bc1eb703824a21631a85
mujtabachandio <mujtabachandio384@gmail.com>
2025-05-04 09:26:58 +0500

    update

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -3,7 +3,7 @@ import { createClient } from '@sanity/client';
 export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID, // from .env.local
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,     // from .env.local
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION, // from .env.local
+  apiVersion: '2023-01-01', // 👈 use a fixed version here
   token: process.env.SANITY_API_WRITE_TOKEN,           // from .env.local
   useCdn: true, // Optional: set to true for faster response times
 });
